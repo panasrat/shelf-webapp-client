12fae24e79f8d88618f3031ca9a5fa953b6320dd
Panas Rattanasuwan <rattanasuwan.pn@gmail.com>
2021-03-31 10:01:10 +0700

    add: SideProfile

--- components/SideProfile/index.jsx
+++ components/SideProfile/index.jsx
@@ -6,12 +6,15 @@ const SideProfile = ({
   user: {
     authenticated,
     loading,
-    credentials: { bio, handle, createdAt },
+    credentials: { bio, handle, imageUrl, createdAt },
   },
 }) => {
   return (
     <>
       <h2>{handle}</h2>
+      <div style={{ width: '300px' }}>
+        <img src={imageUrl} style={{ width: '100% ' }} />
+      </div>
       {bio && <p>{bio}</p>}
       <p>{dayjs(createdAt).format('MMM YYYY')}</p>
     </>
